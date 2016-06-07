80afaee9fcec73b2d4f966628d8ad146099da589
ariadna-ml <ariadna.marin.llaurado@gmail.com>
2016-06-07 09:58:32 +0200

    read rotation and translation

--- crystal.py
+++ crystal.py
@@ -6,32 +6,43 @@ f = open(filename, "r")
 line = f.readline()
 while(not line.startswith("EXPDTA")):
     line = f.readline()
-if line.rfind("CRYSTAL") == -1:
+if (line.rfind("CRYSTAL") == -1) and (line.rfind("DIFFRACTION") == -1):
     raise(ValueError("The input pdb does not correspond to a crystallographic "
                      "structure"))
-while(not line.startswith("CRYST1")):
+# find start crystal structures
+while(line.rfind("REMARK 290       1555") == -1):
     line = f.readline()
-(a, b, c, alpha, beta, gamma, group, g1, g2, g3, z) = line.split()[1:]
-i = 0
-orig_mat = np.zeros((3, 3))
-T = np.zeros(3)
-scale_mat = np.zeros((3, 3))
-S = np.zeros(3)
-line = f.readline()
-while (i < 3):
-    line_split = line.split()
-    orig_mat[i, :] = line_split[1:4]
-    T[i] = line_split[-1]
-    i += 1
-    line = f.readline()
-i = 0
-while (i < 3):
-    line_split = line.split()
-    scale_mat[i, :] = line_split[1:4]
-    S[i] = line_split[-1]
-    i += 1
-    line = f.readline()
-f.close()
+# count structures
+nstr = 0
+while(len(line.split())>2):
+	nstr += 1
+	print(len(line.split()))
+	line = f.readline()
+rot_mat = np.zeros((nstr, 3, 3)) # generate empty coord matrix
+trans_v = np.zeros((nstr, 3))
+
+while(line.rfind("SMTRY1") == -1):
+	line = f.readline()
+	
+# fill rot_mat and trans_v
+n = 0
+dim = 0
+while(len(line.split())>2):	
+	values = line.split()
+	rot_mat[n][dim] = values[4:7]
+	print(rot_mat[n][dim])
+	trans_v[n][dim] = values[7]
+	print(trans_v[n])
+	line = f.readline()
+	if dim == 2:
+		n += 1
+		dim = 0
+	else:
+		dim += 1
+print(rot_mat)
+print(trans_v)
+
 mol = ht.Molecule(filename)
+
 # http://deposit.rcsb.org/adit/docs/pdb_atom_format.html
 # http://pdb101.rcsb.org/learn/guide-to-understanding-pdb-data/introduction
